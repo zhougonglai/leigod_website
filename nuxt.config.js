454b8028e565d8090de2e13914b2f142753122bd
周公来 <zhougonglai@nn.com>
2022-06-23 18:22:11 +0800

    feat: update

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+export default {
+  target: "static",
+  head: {
+    title: "雷神加速器",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    script: [
+      {
+        src: "https://b.alicdn.com/s/polyfill.min.js?features=default,es2015,Intl",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/axios", "@nuxtjs/pwa", "@nuxt/content"],
+  axios: {
+    baseURL: "/",
+  },
+  pwa: {
+    manifest: {
+      lang: "zh-cn",
+    },
+  },
+  content: {},
+  build: {},
+};
