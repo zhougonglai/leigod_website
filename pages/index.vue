5fa6e17f44a9ad7edc5ba3abe4c3460814074fbf
周公来 <zhougonglai@nn.com>
2022-06-24 18:18:40 +0800

    feat: - “首页按钮” 🔘 帧动画重播 - 结束动画微操

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,22 @@
 <template lang="pug">
-main.flex.flex-col.items-center.min-h-screen
-  header.container
+main.flex.flex-col.items-center.min-h-screen.overflow-hidden
+  header.container.flex
     nav.inline-flex
       nuxt-link.py-5(to="/")
-        img(src="~/assets/images/logo.png" alt="logo" width="164" height="42")
+        img(src="~/assets/images/page1/logo.png" alt="logo" width="148" height="41")
+    .flex-1
+    nav.inline-flex
+      button.text-white(v-if="timeline.target" @click="timeline.target.restart") 首页
   section
-  transition(
-    appear
-    enter-class="backdrop-blur-none"
-    enter-active-class="transform"
-    enter-to-class="backdrop-blur"
-    leave-class="backdrop-blur"
-    leave-active-class="transform"
-    leave-to-class="backdrop-blur-none"
-  )
-    #backdrop.absolute.inset-0.w-full.h-full.backdrop-filter.backdrop-blur.z-10(v-show="timeline.backdrop" ref="backdrop")
-  transition
-    #circle.absolute.z-10(v-show="timeline.circle" ref="circle")
-      img(src="@/assets/images/circle.gif")
-  transition
-    #time-1.absolute(ref="title1" v-show="timeline.title1")
-      img.t-1.scale-150.transform(src="@/assets/images/logo.png")
-  transition
-    #time-2.absolute(ref="title2" v-show="timeline.title2")
-      img.t-2(src="@/assets/images/better_title.png")
-  transition
-    .bubble.rounded-full.absolute.origin-center.w-60.h-60(v-show="timeline.bubble" ref='bubble')
+  img.absolute.circle-1.animate-pulse(v-show="timeline.circle1" ref="circle1" src="@/assets/images/page1/circle-1.png" )
+  img.absolute.circle-2.animate-pulse(v-show="timeline.circle2" ref="circle2" src="@/assets/images/page1/circle-2.png" style="animation-delay: .5s;")
+  #circle.absolute.z-10.inset-y-0(v-show="timeline.circle" ref="circle")
+    img.object-center.object-contain(src="@/assets/images/page1/apng/circle.png")
+  #time-1.absolute(ref="title1" v-show="timeline.title1")
+    img.t-1(src="@/assets/images/page1/logo-title.png")
+  #time-2.absolute(ref="title2" v-show="timeline.title2")
+    img.t-2(src="@/assets/images/page2/title2.png")
+  #bubble.rounded-full.absolute.origin-center.w-60.h-60.z-50(v-show="timeline.bubble" ref='bubble')
 </template>
 
 <script>
@@ -36,11 +27,13 @@ export default {
   data() {
     return {
       timeline: {
-        backdrop: false,
         circle: false,
+        circle1: false,
+        circle2: false,
         title1: false,
         title2: false,
         bubble: false,
+        target: '',
       },
     }
   },
@@ -49,45 +42,63 @@ export default {
   },
   methods: {
     async runAnime() {
-      this.timeline.backdrop = true;
-      const t1 = anime.timeline({
+      this.timeline.target = anime.timeline({
         duration: 600,
-        delay: 600,
-        endDelay: 600,
         easing: anime.penner.easeInOutCubic
       })
-      t1.add({
-        targets: this.$refs.backdrop,
+      this.timeline.target.add({
+        targets: [this.$refs.circle],
         opacity: [0, 1],
+        endDelay: 0,
         begin: () => {
-          this.timeline.backdrop = true;
+          this.timeline.circle = true;
+          this.timeline.circle1 = true;
+          this.timeline.circle2 = true;
         },
       }).add({
-        targets: this.$refs.circle,
-        opacity: [0, 1]
-      }).add({
         targets: this.$refs.title1,
-        translateX: [-250, 0],
+        translateX: [-500, 0],
+        delay: 600,
+        endDelay: 200,
         opacity: [0, 1],
-      }).add({
-        targets: this.$refs.title1,
-        translateX: 0,
-      }).add({
-        targets: this.$refs.title1,
-        translateX: [0, 250],
-        opacity: [1, 0],
-      }).add({
-        targets: this.$refs.title2,
-        translateX: [-250, 0],
-        opacity: [0, 1],
-      }, '-=400')
-
-      t1.finished.then(() => {
-        this.$refs.anime.close();
-        this.bubble = true;
-        anime({
+        begin: () => {
+          this.timeline.title1 = true;
+        },
+      }, '-=500')
+        .add({
+          targets: this.$refs.title1,
+          translateX: [0, 500],
+          opacity: [1, 0],
+          delay: 600,
+          complete: () => {
+            this.timeline.title1 = false;
+          },
+        })
+        .add({
+          targets: this.$refs.title2,
+          translateX: [-500, 0],
+          opacity: [0, 1],
+          begin: () => {
+            this.timeline.title2 = true;
+          },
+        }, '-=400')
+        .add({
+          targets: this.$refs.title2,
+          scale: [1, 0.725]
+        })
+        .add({
+          targets: [this.$refs.circle, this.$refs.circle1, this.$refs.circle2],
+          opacity: [1, 0],
+          scale: [1, 0.5],
+          complete: () => {
+            this.timeline.circle = false;
+            this.timeline.circle1 = false;
+            this.timeline.circle2 = false;
+          },
+        })
+        .add({
           targets: this.$refs.bubble,
-          scale: [1, Math.max(Math.ceil(window.innerWidth / 120), Math.ceil(window.innerHeight / 120)) * 1.5],
+          scale: [1, Math.max(Math.ceil(window.innerWidth / 120), Math.ceil(window.innerHeight / 120)) * 1.4],
           easing: anime.penner.easeOutCubic,
           begin: () => {
             this.timeline.bubble = true;
@@ -96,30 +107,62 @@ export default {
             this.timeline.bubble = false;
           }
         })
-      })
-      console.log(t1);
+
+      console.log(this.timeline.target);
     }
   }
 }
 </script>
 <style lang="scss" scoped>
 main {
+  --top-param: 30px;
   background: linear-gradient(180deg, #101010 0%, #2E2E2E 100%);
 }
 
-#backdrop {
-  background: transparent;
+@keyframes pulse {
 
-  &::backdrop {
-    transition: backdrop-filter .6s ease-in-out;
-    backdrop-filter: blur(15px);
-    z-index: 9;
+  0%,
+  100% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: .5;
   }
 }
 
-.bubble {
+.animate-pulse {
+  animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+}
+
+.circle-1 {
+  left: 0;
+  top: 0;
+}
+
+.circle-2 {
+  right: 0;
+  bottom: 0;
+}
+
+
+#circle {
+  top: var(--top-param);
+}
+
+#time-1 {
+  left: calc(50% - 440px);
+  top: calc(50% - 136px + var(--top-param));
+}
+
+#time-2 {
+  left: calc(50% - 598px);
+  top: calc(50% - 88px + var(--top-param));
+}
+
+#bubble {
   box-shadow: 0 0 1.25vw 1.25vw rgba(white, 0.7) inset;
   left: calc(50% - 120px);
-  top: calc(50% - 120px);
+  top: calc(50% - 120px + var(--top-param));
 }
 </style>
